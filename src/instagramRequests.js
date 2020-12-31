100b37d306440afc381ac6046b91712c6c319de3
Omri <omri206attiya@gmail.com>
2020-12-31 17:20:46 +0200

    Initial commit

---
+++ src/instagramRequests.js
@@ -0,0 +1,68 @@
+import fetch from 'node-fetch';
+import config from './user.config.json';
+
+const REQUEST_HEADERS = {headers: {cookie: config.cookie}};
+
+const REQUEST = {
+    followers: {
+        queryUrl: `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=`,
+        key: `edge_followed_by`,
+    },
+    following: {
+        queryUrl: `https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=`,
+        key: `edge_follow`,
+    }
+}
+
+export const instagramRequests = (function () {
+
+    return {
+        getFollowersAndFollowing
+    }
+
+    async function getFollowersAndFollowing() {
+        try {
+            let userId = await getUserId();
+            let followers = await getAccounts(userId, REQUEST.followers);
+            let following = await getAccounts(userId, REQUEST.following);
+            return {followers, following};
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
+    async function getAccounts(userId, request) {
+        let followers = [];
+        let hasNext = true;
+        let after = null;
+        while (hasNext) {
+            await fetch(request.queryUrl + encodeURIComponent(JSON.stringify({
+                id: userId,
+                include_reel: true,
+                fetch_mutual: true,
+                first: 50,
+                after: after
+            })), REQUEST_HEADERS).then(res => res.json()).then(res => {
+                hasNext = res.data.user[request.key].page_info.has_next_page
+                after = res.data.user[request.key].page_info.end_cursor
+                followers = followers.concat(res.data.user[request.key].edges.map(({node}) => {
+                    return {
+                        username: node.username,
+                        fullName: node.full_name,
+                        profilePicUrl: node.profile_pic_url,
+                    }
+                }))
+            })
+        }
+        return followers;
+    }
+
+    async function getUserId() {
+        let res = await fetch(`https://www.instagram.com/${config.username}/?__a=1`, REQUEST_HEADERS);
+        res = await res.json();
+        return res.graphql.user.id;
+    }
+
+
+})();
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import {instagramRequests} from "./instagramRequests.js";
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.get('/api/instagram', async function (req, res) {
+    console.log('request start');
+    let users = await instagramRequests.getFollowersAndFollowing()
+    res.send(users);
+    console.log('request end');
+});
+
+app.listen(3001);
